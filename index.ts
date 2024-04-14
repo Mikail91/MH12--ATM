3a6811221049947788a2c137e3fcfe916a493d22
Mikail91 <159637689+Mikail91@users.noreply.github.com>
2024-04-15 01:02:24 +0500

    Pushing to GITHUB

---
+++ index.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env node 
+
+import inquirer from "inquirer";
+
+
+let myBal = 10000 ;
+let myPin = 12345
+let myCurr = "USD"
+
+const uName = await inquirer.prompt([{
+  message : "Please Enter your Name",
+  type : "String",
+  name : "usName",
+}])
+
+const pin = await inquirer.prompt([{
+  message : "Please enter your pin Code",
+  type : "number",
+  name : "usPin",
+}])
+if (pin.usPin === myPin){
+  console.log ("Welcome", "" + uName.usName)
+  console.log ("Your Current Balance is", + myBal)
+
+  const operator = await inquirer.prompt([{
+  message : "Please Select from the Following",
+  type : "list",
+  name : "usSelection",
+  choices : ["Fast Cash", "Cash Withdrawl", "Balance Inquiry"]
+}])
+if (operator.usSelection === "Fast Cash"){
+  const fCash = await inquirer.prompt([{
+    message : "Please Select Denomination",
+    type : "list",
+    name : "usfCash",
+    choices : ["1","2","5","10","20","50","100"]
+  }])
+  myBal -= fCash.usfCash
+  console.log ("Your Remaining Amount is", "" + myBal )
+}
+else if (operator.usSelection === "Cash Withdrawl"){
+  const usWith = await inquirer.prompt([{
+    message : "Please Enter Denomination",
+    type : "number",
+    name : "usWithdrawl"
+  }])
+  if (usWith.usWithdrawl > myBal){
+    console.log ("Insufficient Balance")
+  }else {
+    myBal -= usWith.usWithdrawl
+    console.log ("Your Remaining Amount is", ""+ myBal)
+  }
+    }
+  else if (operator.usSelection === "Balance Inquiry"){
+    console.log ("Your Balance is ","" + myBal)
+  }
+}else {
+  console.log ("Wrong Pin")
+}
